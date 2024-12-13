e56e648c213170a5147143c97af523055014de29
Marhtini <JohnMartinez90@gmail.com>
2024-12-13 17:49:02 -0500

    Added color and escape key logic.

--- engine.cpp
+++ engine.cpp
@@ -1,24 +1,60 @@
 #include <GLAD\glad.h>
 #include <GLFW\glfw3.h>
+#include "WindowInit.hpp"
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+// Check for Escape
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
 int main() {
+	
 	glfwInit();
 
-	// Set hints for window creation
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Simple Graphics Engine", nullptr, nullptr);
+	GLFWwindow* window = init_window();
 
 	if (window == nullptr) { // Using nullptr over NULL for type safety
-		std::cout << "Failed to create GLFW window.\n" << std::endl;
+		std::cout << "Failed to create GLFW window." << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 
 	glfwMakeContextCurrent(window);
 
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to init GLAD." << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600); // Where to open window x, y then resolution
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Render Loop
+	while (!glfwWindowShouldClose(window)) {
+		
+		// Check for Input
+		processInput(window);
+
+		// Render
+		glClearColor(0.6f, 0.2f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		// Check and Call Events + Swap Bufffer (See Double Buffering)
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 
 }
