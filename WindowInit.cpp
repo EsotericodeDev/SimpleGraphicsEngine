8a64998f4b1a196d40d445f9297179f9bf401dd4
Marhtini <JohnMartinez90@gmail.com>
2024-12-13 17:04:02 -0500

    Moved window init function to a seperate file and finsihed out the rendering code.

---
+++ WindowInit.cpp
@@ -0,0 +1,14 @@
+#include "WindowInit.hpp"
+#include <GLFW\glfw3.h>
+
+GLFWwindow* init_window() {
+	// Set hints for window creation
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create the window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Simple Graphics Engine", nullptr, nullptr);
+
+	return window;
+}
